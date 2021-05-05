c266765c437b95e59806c7ace26111251a8c8a72
Aad Versteden <madnificent@gmail.com>
2021-05-05 13:04:49 +0200

    Initial setup
    This is the initial setup with a PoC version of some of the funtionality
    which we'd like to have.

---
+++ src/index.js
@@ -0,0 +1,69 @@
+/**
+ * Runs some magic to get the real functor in our hands and to bind the right context.
+ */
+function getRealFunctor(functor) {
+  const realFunctor = typeof functor === "string"
+    ? this[functor]
+    : functor;
+
+  if (typeof realFunctor !== "function") {
+    throw `Incorrectly applied @pre, precondition function could not be found (${functor})`;
+  }
+
+  return realFunctor.bind(this);
+}
+
+/**
+ * Applies a pre-condition.
+ *
+ * The functor can be:
+ * - a string: name of a method on this instance.
+ * - a function: function which will yield true/false when called with
+ *     the arguments of the decorated function.
+ *
+ * An error is thrown if the pre-condition does not match.
+ */
+function pre(functor, reason) {
+  return function(target, name, descriptor) {
+    const original = descriptor.value;
+
+    descriptor.value = function(...args) {
+      const realFunctor = getRealFunctor.call(this, functor);
+
+      if (!realFunctor.apply(this, args))
+        throw reason;
+      else
+        return original.apply(this, args);
+    };
+
+    return descriptor;
+  };
+}
+
+/**
+ * Applies a post-condition.
+ *
+ * - a string: name of a method on this instance.
+ * - a function: function which will yield true/false when called with
+ *     the arguments of the decorated function.
+ *
+ * An error is thrown if the post-condition does not match.
+ */
+function post(functor, reason) {
+  return function(target, name, descriptor) {
+    const original = descriptor.value;
+
+    descriptor.value = function(...args) {
+      const realFunctor = getRealFunctor.call(this, functor);
+      const originalResult = original.apply(this, args);
+      if (!realFunctor.apply(this, args))
+        throw reason;
+      else
+        return originalResult;
+    };
+
+    return descriptor;
+  };
+}
+
+export { pre, post };
